7603124101ddcaa43793b60c1b2406a76d7a370e
Matt Bond <matt+github@codefire.io>
2018-12-07 11:45:54 -0500

    Day1 part 2 first pass

--- days/1.js
+++ days/1.js
@@ -1,7 +1,33 @@
 "use strict";
 
-module.exports.run = function(offsetsString, initial = 0) {
-	let offsets = offsetsString.split(',').map( x => Number(x) );
+function prepareInput(offsets) {
+	return offsets.split(',').map( x => Number(x) );
+}
+
+const run1 = function(offsetsString, initial = 0) {
+	const offsets = prepareInput(offsetsString);
 	return offsets.reduce((x, y) => x + y, initial);
 }
+
+
+
+const run2 = function(offsetsString) {
+	const offsets = prepareInput(offsetsString);
+	let accumulator = 0;
+	const seen = new Map();
+	seen.set(accumulator, null);
+
+	while (true) {
+		for(let o of offsets) {
+			accumulator += o;
+			if (seen.has(accumulator)) {
+				return accumulator;
+			} else {
+				seen.set(accumulator, null);
+			}
+		}
+	}
+}
+
+module.exports = { run1, run2 };
 
--- tests/1.js
+++ tests/1.js
@@ -3,34 +3,45 @@ import day from '../days/1';
 
 
 test('basic test 1', t => {
-	t.is(day.run("+1"), 1);
+	t.is(day.run1("+1"), 1);
 });
 
 test('basic test 2', t => {
-	t.is(day.run("-2", 1), -1);
+	t.is(day.run1("-2", 1), -1);
 });
 
 test('basic test 3', t => {
-	t.is(day.run("+3", -1), 2);
+	t.is(day.run1("+3", -1), 2);
 });
 
 test('basic test 4', t => {
-	t.is(day.run("+1", 2), 3);
+	t.is(day.run1("+1", 2), 3);
 });
 
-test('example 1', t => {
-	t.is(day.run("+1, +1, +1"), 3);
+test('example 1.1.1', t => {
+	t.is(day.run1("+1, +1, +1"), 3);
 });
 
-test('example 2', t => {
-	t.is(day.run("+1, +1, -2"), 0);
+test('example 1.1.2', t => {
+	t.is(day.run1("+1, +1, -2"), 0);
 });
 
-test('example 3', t => {
-	t.is(day.run("-1, -2, -3"), -6);
+test('example 1.1.3', t => {
+	t.is(day.run1("-1, -2, -3"), -6);
 });
 
-
+test('example 1.2.1', t => {
+	t.is(day.run2("+1, -1"), 0);
+});
+test('example 1.2.2', t => {
+	t.is(day.run2("+3, +3, +4, -2, -4"), 10);
+});
+test('example 1.2.3', t => {
+	t.is(day.run2("-6, +3, +8, +5, -6"), 5);
+});
+test('example 1.2.4', t => {
+	t.is(day.run2("+7, +7, -2, -7, -4"), 14);
+});
 
 /*
  * For example, if the device displays frequency changes of +1, -2, +3, +1, then starting from a frequency of zero, the following changes would occur:
@@ -46,4 +57,11 @@ Here are other example situations:
 +1, +1, +1 results in  3
 +1, +1, -2 results in  0
 -1, -2, -3 results in -6
+*/
+
+/*
+ * +1, -1 first reaches 0 twice.
++3, +3, +4, -2, -4 first reaches 10 twice.
+-6, +3, +8, +5, -6 first reaches 5 twice.
++7, +7, -2, -7, -4 first reaches 14 twice.
 */
