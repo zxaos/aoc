6981f0a830901dc9f497268465efff1941ba2031
Matt Bond <matt+git@codefire.io>
2019-12-01 16:01:52 -0500

    add 1-2 The Tyranny of the Rocket Equation

--- days/1.exs
+++ days/1.exs
@@ -1,11 +1,21 @@
 defmodule DayOne do
-  def fuel_for_weight (weight) do
+  def raw_fuel_for_weight (weight) do
     div(weight, 3) - 2
   end
 
   def raw_fuel_for_modules (modules) do
-    Enum.sum( Enum.map modules, &fuel_for_weight/1 )
+    Enum.sum( Enum.map modules, &raw_fuel_for_weight/1 )
   end
+
+  def compounding_fuel_for_modules (modules) do
+    Enum.sum( Enum.map modules, &compounding_fuel/1 )
+  end
+
+  def compounding_fuel(weight) when weight >= 9 do # 9/3 - 2 = 1, discard anything 0 or smaller
+    current = raw_fuel_for_weight(weight)
+    current + compounding_fuel(current)
+  end
+  def compounding_fuel(_), do: 0
 
 end
 
@@ -15,3 +25,4 @@ parsed_input = with {:ok, contents} = File.read("1.input") do
 end
 
 IO.puts "Fuel for modules alone: #{DayOne.raw_fuel_for_modules parsed_input }"
+IO.puts "Fuel for modules + fuel: #{DayOne.compounding_fuel_for_modules parsed_input }"
