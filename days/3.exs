0eea5f8098dd41286483b31c6c124f3b3159dbb6
Matt Bond <matt+git@codefire.io>
2019-12-21 21:46:13 -0500

    add 3-2 Crossed Wires

--- days/3.exs
+++ days/3.exs
@@ -14,11 +14,20 @@ end
 
 defmodule DayThree do
   def find_nearest_intersection (wires) do
-    [first, second] = Enum.map(wires, &coordinates_from_wire/1)
-    first -- (first -- second)   # instersection of wire coordinates
-    |>List.delete(%Coordinate{}) # exclude origin
-    |>Enum.map(&manhattan_distance/1)
-    |>Enum.min
+    [{first, first_map}, {second, second_map}] = Enum.map(wires, &coordinates_from_wire/1)
+    intersections = first -- (first -- second)   # instersection of wire coordinates
+
+    manhattan_min = Enum.min_by(intersections, &manhattan_distance/1)
+    IO.puts "Minimum by manhattan distance:"
+    IO.inspect manhattan_min
+    IO.inspect manhattan_distance manhattan_min
+
+    wirelength_min = Enum.min_by(intersections, &( first_map[&1] + second_map[&1]))
+
+    IO.puts "Minimum by wire length:"
+    IO.inspect wirelength_min
+    IO.inspect first_map[wirelength_min] + second_map[wirelength_min]
+
   end
 
   def coordinates_from_wire(wire = [_direction | _tail], location \\ %Coordinate{}) do
@@ -28,27 +37,35 @@ defmodule DayThree do
       {end_loc, new_coords} = result
       {end_loc, generated_wire_coordinates ++ new_coords}
     end)
-    coords
+
+    wire_distance_map = Enum.with_index(coords, 1)
+      |> Enum.reverse # into replaces duplicate vaules, but we want to prefer lower counts
+      |> Enum.into(%{})
+
+    { coords, wire_distance_map }
   end
 
+  # the start values are all offset by one here because we don't need to include the starting
+  # coordinate in the map - either it's the origin, or it was the last generated coordinate from
+  # the previous segment
   def run_wire(%{x: x, y: y}, "U", distance) do
     endloc = %Coordinate{x: x, y: y + distance}
-    coords = Enum.map(y..(y + distance), &(%Coordinate{x: x, y: &1}))
+    coords = Enum.map(y+1..(y + distance), &(%Coordinate{x: x, y: &1}))
     {endloc, coords}
   end
   def run_wire(%{x: x, y: y}, "D", distance) do
     endloc = %Coordinate{x: x, y: y - distance}
-    coords = Enum.map(y..(y - distance), &(%Coordinate{x: x, y: &1}))
+    coords = Enum.map(y-1..(y - distance), &(%Coordinate{x: x, y: &1}))
     {endloc, coords}
   end
   def run_wire(%{x: x, y: y}, "R", distance) do
     endloc = %Coordinate{x: x + distance, y: y}
-    coords = Enum.map(x..(x + distance), &(%Coordinate{x: &1, y: y}))
+    coords = Enum.map(x+1..(x + distance), &(%Coordinate{x: &1, y: y}))
     {endloc, coords}
   end
   def run_wire(%{x: x, y: y}, "L", distance) do
     endloc = %Coordinate{x: x - distance, y: y}
-    coords = Enum.map(x..(x - distance), &(%Coordinate{x: &1, y: y}))
+    coords = Enum.map(x-1..(x - distance), &(%Coordinate{x: &1, y: y}))
     {endloc, coords}
   end
 
@@ -64,4 +81,4 @@ parsed_input = with {:ok, contents} = File.read(input_file) do
   |> Enum.map( fn (wire) -> Enum.map(wire, &WireDirection.new/1) end)
 end
 
-IO.inspect DayThree.find_nearest_intersection(parsed_input)
+DayThree.find_nearest_intersection(parsed_input)
