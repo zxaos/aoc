5fcb57daffdfe64ff0c8e36045cb3ebe0d46ed09
Matt Bond <matt+git@codefire.io>
2024-01-10 18:28:21 -0500

    make 1 use shared lib

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,21 +1,14 @@
-use std::{fs::File, io::Read, path::Path};
+use aoc_2015::aoc_io;
 
 fn main() {
-    let path = Path::new("inputs/input.1.txt");
-    let mut file = File::open(&path).expect("Failed to open input");
-
-    let mut input = String::new();
-    file.read_to_string(&mut input)
-        .expect("Failed to read input");
-
+    let input = aoc_io::get_aoc_input_as_string(1);
     let result = count_floors(input);
-    println!("1: {}", result.0);
-    println!("2: {}", result.1);
+    aoc_io::put_aoc_output((Some(result.0), Some(result.1)));
 }
 
-fn count_floors(instructions: String) -> (i32, u32) {
+fn count_floors(instructions: String) -> (i64, i64) {
     let mut floor = 0;
-    let mut first_basement: Option<u32> = None;
+    let mut first_basement: Option<i64> = None;
 
     for (i, c) in instructions.trim().chars().enumerate() {
         match c {
@@ -24,7 +17,7 @@ fn count_floors(instructions: String) -> (i32, u32) {
             _ => panic!("Unexpected input character"),
         }
         if first_basement.is_none() && floor < 0 {
-            first_basement = Some(i as u32 + 1);
+            first_basement = Some(i as i64 + 1);
         }
     }
     return (floor, first_basement.unwrap());
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod aoc_io {
+    use std::{fs::File, io::Read, path::Path};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    pub fn get_aoc_input_as_string(day: u8) -> String {
+        let pathstring = format!("inputs/input.{}.txt", day);
+        let path = Path::new(&pathstring);
+        let mut file = File::open(&path).expect("Failed to open input");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut input = String::new();
+        file.read_to_string(&mut input)
+            .expect("Failed to read input");
+        input
+    }
+
+    pub fn put_aoc_output(results: (Option<i64>, Option<i64>)) {
+        maybe_print_solution(results.0, 1);
+        maybe_print_solution(results.1, 2);
+    }
+
+    fn maybe_print_solution(result: Option<i64>, part: u8) {
+        if let Some(r) = result {
+            println!("{}: {}", part, r);
+        } else {
+            println!("No solution (yet) for part {part}.")
+        }
     }
 }
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
