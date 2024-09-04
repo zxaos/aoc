32e7c2eca15f4965c019c00ab7a3ed04c12d8b74
Matt Bond <matt+git@codefire.io>
2024-09-03 21:57:52 -0400

    add 19 part 1

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,4 +1,8 @@
-use miette::{Diagnostic, IntoDiagnostic, Result};
+use std::collections::{HashMap, HashSet};
+
+use aoc_2015::aoc_io::Solution;
+use itertools::Itertools;
+use miette::{bail, Diagnostic, IntoDiagnostic, Result};
 use thiserror::Error;
 
 #[derive(Error, Debug, Diagnostic)]
@@ -8,10 +12,64 @@ enum Err19 {
 }
 
 type Replacement = (String, Vec<String>);
+type ReplacementMap = HashMap<String, Vec<Vec<String>>>;
 
 fn main() -> Result<()> {
-    let raw_input = aoc_2015::aoc_io::get_collected_input_as_lines(19);
+    let mut raw_input = aoc_2015::aoc_io::get_collected_input_as_lines(19);
+    let mut solution: Solution<usize> = Solution::new();
+
+    let target: Vec<String>;
+    loop {
+        if let Some(line) = raw_input.pop() {
+            match line.as_str() {
+                "" => {
+                    println!("Skipping empty line");
+                    continue;
+                }
+                _ => {
+                    target = to_elements(&line);
+                    break;
+                }
+            }
+        } else {
+            bail!("Input was only empty lines")
+        };
+    }
+    // There should be an empty line after the main molecule input
+    raw_input.pop();
+
+    let mut replacements: ReplacementMap = HashMap::with_capacity(raw_input.len());
+    for r in raw_input.into_iter().map(parse_replacement) {
+        if let Ok((from, to)) = r {
+            match replacements.get_mut(&from) {
+                Some(record) => record.push(to),
+                None => {
+                    replacements.insert(from, vec![to]);
+                }
+            }
+        } else {
+            bail!("unpacked replacement was bad")
+        }
+    }
+
+    solution[0].solution = Some(count_unique_replacements(target, replacements));
+    solution.print();
     Ok(())
+}
+
+fn count_unique_replacements(target: Vec<String>, replacements: ReplacementMap) -> usize {
+    let mut results: HashSet<String> = HashSet::with_capacity(target.len() * 2);
+    for (idx, segment) in target.iter().enumerate() {
+        if let Some(rs) = replacements.get(segment) {
+            for current in rs {
+                let before = target[0..idx].iter().join("");
+                let current = current.iter().join("");
+                let after = target[idx + 1..].iter().join("");
+                results.insert(format!("{before}{current}{after}"));
+            }
+        }
+    }
+    results.len()
 }
 
 // Turns "Ca => SiRnFYFAr" into a replacement record
@@ -46,6 +104,7 @@ fn to_elements(i: &str) -> Vec<String> {
     reverse(i)
         .split_inclusive(char::is_uppercase)
         .map(reverse)
+        .rev() // Since we reversed the initial string, we'd get the results in reverse order if not this.
         .collect()
 }
 
@@ -56,9 +115,25 @@ mod test {
     fn test_parse() -> Result<()> {
         let r = parse_replacement("Ca => SiRnFYFAr")?;
         assert_eq!(r.0, "Ca");
-        let mut correct = vec!["Si", "Rn", "F", "Y", "F", "Ar"];
-        correct.reverse();
-        assert_eq!(r.1, correct);
+        assert_eq!(r.1, vec!["Si", "Rn", "F", "Y", "F", "Ar"]);
         Ok(())
+    }
+
+    #[test]
+    fn test_replacements() {
+        let mut replacements: ReplacementMap = HashMap::with_capacity(3);
+        replacements.insert(
+            "H".to_string(),
+            vec![
+                vec!["H".to_string(), "O".to_string()],
+                vec!["O".to_string(), "H".to_string()],
+            ],
+        );
+        replacements.insert(
+            "O".to_string(),
+            vec![vec!["H".to_string(), "H".to_string()]],
+        );
+        let target = vec!["H".to_string(), "O".to_string(), "H".to_string()];
+        assert_eq!(count_unique_replacements(target, replacements), 4);
     }
 }
